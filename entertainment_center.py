a0bb77a38349f33ff8ae4a5c9355c279584581a1
Install User <install@researchstudio.at>
2018-06-06 12:14:04 +0200

    update python code style

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,7 +4,7 @@ import fresh_tomatoes
 
 # create Movie object
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys.",
-            "https://vignette.wikia.nocookie.net/disney/images/8/80/Toy_Story_-_Poster.jpg",
+						"https://vignette.wikia.nocookie.net/disney/images/8/80/Toy_Story_-_Poster.jpg",
             "https://www.youtube.com/watch?v=rNk1Wi8SvNc")
 
 shrek = media.Movie("Shrek", "An ogre goes on an adventure",
@@ -12,11 +12,11 @@ shrek = media.Movie("Shrek", "An ogre goes on an adventure",
         "https://www.youtube.com/watch?v=W37DlG1i61s")
 
 californiaction = media.Movie("Californication", "An author living in california",
-"https://images-na.ssl-images-amazon.com/images/I/81B9XVkMJ9L._SY445_.jpg",
-"https://www.youtube.com/watch?v=VfxF6UkoX-g")
+									"https://images-na.ssl-images-amazon.com/images/I/81B9XVkMJ9L._SY445_.jpg",
+									"https://www.youtube.com/watch?v=VfxF6UkoX-g")
 
 print ("entertainment started!")
 
-movies =[toy_story, shrek, californiaction]
-fresh_tomatoes.open_movies_page(movies) # invoke method open_movies_page() to display provided movies
-
+movies = [toy_story, shrek, californiaction]
+# invoke method open_movies_page() to display provided movies
+fresh_tomatoes.open_movies_page(movies)
